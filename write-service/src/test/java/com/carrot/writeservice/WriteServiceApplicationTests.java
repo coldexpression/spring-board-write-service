e57d4cc412789b7a47d1bb2167a8403e8e9d7ef7
coldexpression <uni_vers_@naver.com>
2022-04-14 01:04:39 +0900

    초기 설정

---
+++ write-service/src/test/java/com/carrot/writeservice/WriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrot.writeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
